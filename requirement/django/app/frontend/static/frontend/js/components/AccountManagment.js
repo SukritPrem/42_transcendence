7efb0da7825a79c31b859a7095fd3c7a13d7b77c
lockdoor <lockdoor@gmail.com>
2024-08-26 14:56:27 +0700

    work on update avatar

--- requirement/django/app/frontend/static/frontend/js/components/AccountManagment.js
+++ requirement/django/app/frontend/static/frontend/js/components/AccountManagment.js
@@ -1,9 +1,10 @@
-import { getUserAvatar } from "./Utils.js";
+import { getCSRFToken, getUserAvatar, getUserId } from "./Utils.js";
 export class AccountManagment extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({ mode: "open" });
 		this.shadowRoot.innerHTML = this.template();
+	  this.uploadAvatar =	this.uploadAvatar.bind(this)
 	}
 
 	template = () => {
@@ -26,15 +27,16 @@ export class AccountManagment extends HTMLElement {
 							onerror="this.onerror=null; this.src='${window.location.origin+"/user-media/avatars/default.png"}';">
 					</div>
 					<form id="formAvatar">
-					<input id="avatarUpload" class="input-img d-none"
-						type="file" value=""
-						name="avatar" accept="image/*">
-					<div class="ms-0">
-						<button type="submit"
-							class="d-flex align-items-center justify-content-center gap-2 border-0" 
-							id="uploadBtn">Upload Image
-						</button>
-					</div>
+						<input type="text" class="d-none" name="user_id" value="${getUserId()}">
+						<input id="avatarInput" class="input-img d-none"
+							type="file" value=""
+							name="avatar" accept="image/*">
+						<div class="ms-0">
+							<button type="submit"
+								class="d-flex align-items-center justify-content-center gap-2 border-0" 
+								id="uploadBtn">Upload Image
+							</button>
+						</div>
 					</form>
 				<div>
 			</div>
@@ -43,23 +45,53 @@ export class AccountManagment extends HTMLElement {
 
 	async uploadAvatar(e) {
 		e.preventDefault()
-		if (avatarInput.value) {
-			alert('should upload')
+		try {
+			const avatarInput = this.shadowRoot.getElementById('avatarInput')
+			if (!avatarInput.value) {
+				alert('Click your avatar to choose new one before upload.')
+				return
+			}
+
+			const csrfToken = getCSRFToken();
+			if (!csrfToken) {
+				throw new Error("CSRF token not found");
+			}
+
+			const form = e.target
+			const formData = new FormData(form)
+
+			const response = await fetch(`/api/users/update_avatar`, {
+				method: 'POST',
+				credentials: "same-origin",
+				headers: {
+					"X-CSRFToken": csrfToken
+				},
+				body: formData,
+			});
+
+			const result = await response.json();
+			if (response.status == 201){
+				alert("Avatar changed!!")
+				console.log(result)
+			}
+			else
+				throw new Error(`${response.status} ${response.statusText} ${result.error}`)
+
 		}
-		else {
-			alert('Click your avatar to choose new one before upload.')
+		catch (error) {
+			console.error('Error uploadAvatar:', error);
 		}
 	}
 
 	connectedCallback() {
 		const profileImg = this.shadowRoot.getElementById("profileImg")
-		const avatarInput = this.shadowRoot.getElementById('avatarUpload')
+		const avatarInput = this.shadowRoot.getElementById('avatarInput')
 		const formAvatar = this.shadowRoot.getElementById('formAvatar')
 		profileImg.addEventListener('click', ()=>{
 			avatarInput.click()
 		})
 		avatarInput.addEventListener('change', ()=>{
-			console.log(avatarInput.value)
+			// console.log(avatarInput.value)
 			if (avatarInput.value)
 				profileImg.src = URL.createObjectURL(avatarInput.files[0]);
 			else
